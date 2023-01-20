b9517add11427db415f5b87afe954dc894ec5fbe
remolovdplasse <Remolovdplasse@gmail.com>
2023-01-20 12:54:23 +0100

    Structured folders + started new layout

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'header.php';
+
+    /* Content for the page below */
+?>
+
+
+<div>
+    Welcome to Betternose!
+    <p>
+        Een betere en mooiere versie van de Datanose website :)
+    </p>
+</div>
+
+
+<?php
+    include_once 'footer.php';
+?>
