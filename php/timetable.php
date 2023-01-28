f0c89dac89efc6b54f87419418745c43487b9a9d
remolovdplasse <Remolovdplasse@gmail.com>
2023-01-28 17:56:01 +0100

    merge

--- php/timetable.php
+++ php/timetable.php
@@ -4,8 +4,17 @@
     /* Content for the page below */
 ?>
 
-
-
+    <div class="content-wrapper personal-timetable">
+        <div class="flex--column section-header">
+            <span class="header">Your Timetable 📅</span>
+        </div>
+        <div class="flex--row content-wrapper">
+            <div class="flex--column wrapper--left">
+                <div class="content-box periods"></div>
+            </div>
+            <div class="flex--column wrapper--right"></div>
+        </div>
+    </div>
 
 
 <?php
